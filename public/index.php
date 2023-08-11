ad26bbd53562ce91681ed2ea27e596caeca62bf4
vgo <vgo@ccabe.com>
2023-08-11 16:07:02 +0200

    The 'symfony console' command & the server_version

--- public/index.php
+++ public/index.php
@@ -5,6 +5,5 @@ use App\Kernel;
 require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    dd($_SERVER);
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
