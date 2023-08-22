e504c9fe3fe371ea0a765f573062e6b0198f7d94
vgo <vgo@ccabe.com>
2023-08-22 11:59:10 +0200

    chap14 - The request object

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -6,6 +6,7 @@ use App\Entity\VinylMix;
 use App\Repository\VinylMixRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -47,10 +48,24 @@ class MixController extends AbstractController
 
     //param converter : query for a single object
     #[Route('mix/{id}', name: 'app_mix_show')]
-    public function show(VinylMix $mix)
+    public function show(VinylMix $mix): Response
     {
         return $this->render('mix/show.html.twig', [
             'mix' => $mix,
         ]);
+    }
+
+    #[Route('/mix/{id}/vote', name: 'app_mix_vote', methods: ['POST'])]
+    public function vote(VinylMix $mix, Request $request): Response
+    {
+        // if not direction pass up.
+        $direction = $request->request->get('direction', 'up');
+        if ($direction === 'up') {
+            $mix->setVotes($mix->getVotes() + 1);
+        } else {
+            $mix->setVotes($mix->getVotes() - 1);
+        }
+
+        dd($mix);
     }
 }
